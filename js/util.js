24a08f22ea8019c43deefdfe0f7ca802e54405b6
luiighi2693 <luis2693>
2016-10-27 17:43:11 -0400

    refactorizacion de la funcion exit

--- js/util.js
+++ js/util.js
@@ -1,6 +1,18 @@
 /**
  * Created by LRodriguez on 27/10/2016.
  */
+$( document ).ready(function() {
+    $("#nav").load("nav.html");
+    $("#footer").load("footer.html");
+
+    if (sessionStorage.getItem("usuario")==null){
+        setTimeout(function(){
+            alert("Usted no ha iniciado sesión");
+            window.location = "login.html";
+        }, 1500);
+    }
+});
+
 function exit() {
     sessionStorage.removeItem("usuario");
     sessionStorage.removeItem("rol");
