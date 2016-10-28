cd6d855073fb68bee8f82c2d0a679a1656f141a2
luighi2693 <luiscarvajal2693@gmail.com>
2016-10-28 02:20:11 -0400

    refactorizacion de la funcion de exit

--- js/init.js
+++ js/init.js
@@ -2,6 +2,12 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    console.log("hola");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function exit() {
+  sessionStorage.removeItem("usuario");
+  sessionStorage.removeItem("rol");
+  window.location = "login.html";
+}
