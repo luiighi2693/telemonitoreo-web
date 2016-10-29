dba03779d6c259e03f2c30d593a79d56932e3f44
luighi2693 <luiscarvajal2693@gmail.com>
2016-10-29 01:25:47 -0400

    refactorizacion del create

--- js/init.js
+++ js/init.js
@@ -22,3 +22,22 @@ function getFecha() {
   var fecha = new Date();
   return fecha.getDate()+"/"+(fecha.getMonth()+1)+"/"+fecha.getFullYear()+"  "+fecha.getHours()+":"+(fecha.getMinutes() < 10 ? "0"+fecha.getMinutes() : fecha.getMinutes())+":"+(fecha.getSeconds() < 10 ? "0"+fecha.getSeconds() : fecha.getSeconds());
 }
+
+function save(uri, headers, urlRollBack, type){
+  $.ajax({
+    url: uri,
+    type: type,
+    dataType: 'json',
+    headers: headers,
+    contentType: 'application/json; charset=utf-8',
+    success: function (data) {
+      document.getElementById("spinner").setAttribute("class", "spinnerHidden");
+      if(urlRollBack!=null){
+        window.location= urlRollBack;
+      }
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+}
